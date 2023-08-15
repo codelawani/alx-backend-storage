c3ae604ac135af43d18b8112aca063c815b490ad
Nico <lawani.gk@gmail.com>
2023-08-15 12:31:06 +0100

    make file executable

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,9 +2,7 @@
 """Python script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-
-def log_stats():
-    """Log stats"""
+if __name__ == '__main__':
     with MongoClient() as client:
         nginx_coll = client.logs.nginx
         print(nginx_coll.count_documents({}), 'logs')
@@ -16,7 +14,3 @@ def log_stats():
         status_count = nginx_coll.count_documents(
             {'method': 'GET', 'path': '/status'})
         print(f'{status_count} status check')
-
-
-if __name__ == '__main__':
-    log_stats()
