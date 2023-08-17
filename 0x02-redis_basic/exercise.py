dde82d33fc685d85bb9acc078a03ffdc79ac0f4c
Nico <lawani.gk@gmail.com>
2023-08-17 23:02:04 +0100

    add docstrings

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,62 @@
 #!/usr/bin/env python3
 """Redis Cache Class"""
-from typing import Any, Callable, Union
+from typing import Callable, Any, Union
+from functools import wraps
 import redis
 from uuid import uuid4
-from functools import wraps
 
 
 def count_calls(f: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Decorator that counts the number of times a function is called and increments the count in Redis.
+
+    Args:
+        f (Callable): The function to be wrapped.
+
+    Returns:
+        Callable: The wrapper function.
+    """
     @wraps(f)
     def wrapper(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        Wrapper function that increments the call count and calls the original function.
+
+        Args:
+            self: Instance of the class.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Any: Result of the original function call.
+        """
         self._redis.incrby(f.__qualname__, 1)
         return f(self, *args, **kwargs)
     return wrapper
 
 
 def call_history(f: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Decorator that records the inputs and outputs of a function call in Redis.
+
+    Args:
+        f (Callable): The function to be wrapped.
+
+    Returns:
+        Callable: The wrapper function.
+    """
     @wraps(f)
     def wrapper(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        Wrapper function that records function inputs and outputs in Redis and calls the original function.
+
+        Args:
+            self: Instance of the class.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Any: Result of the original function call.
+        """
         in_key = f'{f.__qualname__}:inputs'
         out_key = f'{f.__qualname__}:outputs'
         self._redis.rpush(in_key, str(*args))
@@ -27,29 +67,85 @@ def call_history(f: Callable[..., Any]) -> Callable[..., Any]:
 
 
 class Cache:
+    """
+    A class that provides caching functionality using Redis and decorators.
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the Cache instance.
+
+        Returns:
+            None
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, float, int, bytes]) -> str:
+        """
+        Stores data in the cache and returns a unique key.
+
+        Args:
+            data (Union[str, float, int, bytes]): The data to be stored.
+
+        Returns:
+            str: The unique key associated with the stored data.
+        """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Callable = None) -> Union[str, float, int, bytes]:
+        """
+        Retrieves data from the cache using the given key.
+
+        Args:
+            key (str): The key associated with the cached data.
+            fn (Callable, optional): A function to transform the retrieved value. Defaults to None.
+
+        Returns:
+            Union[str, float, int, bytes]: The retrieved data.
+        """
         value = self._redis.get(key)
         return fn(value) if fn else value
 
     def get_str(self, key: str) -> str:
+        """
+        Retrieves a string value from the cache using the given key.
+
+        Args:
+            key (str): The key associated with the cached string.
+
+        Returns:
+            str: The retrieved string value.
+        """
         return self.get(key, fn=lambda v: v.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
+        """
+        Retrieves an integer value from the cache using the given key.
+
+        Args:
+            key (str): The key associated with the cached integer.
+
+        Returns:
+            int: The retrieved integer value.
+        """
         return self.get(key, fn=lambda v: int(v))
 
 
 def replay(f):
+    """
+    Replays the history of a function's calls and their inputs and outputs.
+
+    Args:
+        f: The function whose history will be replayed.
+
+    Returns:
+        None
+    """
     key = f.__qualname__
     in_key = key + ':inputs'
     out_key = key + ':outputs'
@@ -62,6 +158,7 @@ def replay(f):
         output = output.decode('utf-8')
         input = input.decode('utf-8')
         print(f"Cache.store(*({input},)) -> {output}")
+
 
 # cache = Cache()
 
